a850285693c0e82c4a3a0f1d45ad786b1f1cd787
josh-mcd05 <mcdonald.josh@northeastern.edu>
2026-02-24 15:16:40 -0500

    Updated original test

--- scripts/test1.py
+++ scripts/test1.py
@@ -1,11 +1,32 @@
 import gym
 import minerl
 env = gym.make('MineRLBasaltFindCave-v0')
+env.seed(88292929494)
 obs = env.reset()
 print('Environment created successfully!')
 print('Observation space:', env.observation_space)
 print('Action space:', env.action_space)
+
+
+for i in range(1000):
+    action = env.action_space.sample()
+    # Just look around and walk forward
+    #action["camera"] = [0, 3]      # turn camera right
+    action["ESC"] = 0
+
+
+    obs, reward, done, info = env.step(action)
+    env.render()
+
+    #print(f"Step {i}, reward: {reward}, done: {done}")
+    if done:
+        print("Episode ended")
+        break
+
 env.close()
 
-## Need to change venv to 3.10 so it will install correctly
-## Should write README setup for how to setup minerl
+## Changes made to run: 
+# in MCP-Reborn/launchClient.sh:
+# - change final java line to: java -XstartOnFirstThread -Xmx$maxMem -jar $fatjar --envPort=$port
+#   - ** only on mac
+    
